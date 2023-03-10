85110eadc06acfac12b492e3f5118284840f6de0
Zintle Mbulawa <105041576+Zintle95@users.noreply.github.com>
2023-03-10 14:46:35 +0200

    Add files via upload

---
+++ My portfolio/main.js
@@ -0,0 +1,8 @@
+window.onscroll=function() {scrollFuction()};
+function scrollfuction(){
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+  document.getElementById("navbar");
+}
+  else {document.getElementById("navbar");
+}
+}
